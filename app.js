e8e38e4213935f053f06e983b7a14f1b2f2e987c
Len <leonard.wu92@imperial.ac.uk>
2013-05-25 06:19:08 +0100

    New favicon; Allow commandline override of server port

--- app.js
+++ app.js
@@ -8,15 +8,17 @@ var express = require('express')
   , user = require('./routes/user')
   , http = require('http')
   , path = require('path')
-  , config = require('./config');
+  , config = require('./config')
+  , util = require('./lib/util')
+  , DEFAULT_PORT = 8880;
 
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || config.Port || 8880);
-app.set('views', __dirname + '/views');
+app.set('port', util.cli.port || config.Port || DEFAULT_PORT);
+app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'jade');
-app.use(express.favicon());
+app.use(express.favicon(path.join(__dirname, '/public/i/favicon.ico')));
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
@@ -28,13 +30,14 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-var adminPath = path.join("/", config.Admin, "/*");
-var adminBasePath = path.dirname(adminPath);
+var adminPath = path.join("/", config.Admin, "/*"),
+adminBasePath = path.dirname(adminPath);
+
 app.get(adminBasePath, routes.index);
 app.get(adminPath, routes.index);
 app.get('/*', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+  console.log('Athex listening on port ' + app.get('port'));
   console.log('Admin URL http://localhost:' + app.get('port')  + adminBasePath);
 });
